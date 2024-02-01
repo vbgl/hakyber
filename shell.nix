58e81197bf15fea6c94fccd4584e87b82d7aebd3
Vincent Laporte <Vincent.Laporte@inria.fr>
2024-02-01 10:07:45 +0100

    2024-02-01T10:07:45+01:00

--- shell.nix
+++ shell.nix
@@ -1,13 +1,23 @@
 with import <nixpkgs> {};
 
+let ec = (easycrypt.overrideDerivation (_: {
+  src = fetchFromGitHub {
+    owner = "EasyCrypt";
+    repo = "easycrypt";
+    rev = "f7992e1fe5a443a9dcbce2941f708ea7bc78f6e0";
+    hash = "sha256-CdgF2bFzUPNMQoGCOsJaqKp4pDBMqtFZXB0y1Miwm2c=";
+  };
+})).override { ocamlPackages = ocaml-ng.ocamlPackages_4_14; }
+; in
+
 mkShell {
   packages = [
-    jasmin-compiler
-    easycrypt
+    ec
     alt-ergo
     cvc4
     z3
   ];
 
-  JASMINC = "jasminc";
+  JASMINC = "${jasmin-compiler.bin}/bin/jasminc";
+  EC_RDIRS = "Jasmin:${jasmin-compiler.lib}/lib/jasmin/easycrypt";
 }
